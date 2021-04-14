a23592df5456a4cb82c26a6c271347ac207f5cb7
aseel <aseel@aseel.localdomain>
2021-04-14 15:39:14 +0200

    updated ListTest

--- tests/Feature/ListTest.php
+++ tests/Feature/ListTest.php
@@ -2,21 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ListTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testViewList()
+    public function testViewListAfterLoginUser()
     {
-        $response = $this->get('/your-list');
+        $user = new User();
+        $user->name = 'Mr Robot';
+        $user->email = 'example@yrgo.se';
+        $user->password = Hash::make('1235678');
+        $user->save();
 
-        $response->assertStatus(200);
+        $response = $this
+            ->followingRedirects()
+            ->post('login', [
+                'email' => 'example@yrgo.se',
+                'password' => '1235678',
+            ]);
+
+        $response = $this->actingAs($user)->get('/your-list');
+        if ($response->assertStatus(200)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
     }
 }
